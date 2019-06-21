846738e001b741c5a2fddce3df95309c4181cc02
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-21 12:36:44 -0400

    carousel works but is not animated

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,20 @@
-class Carousel {
+/* Carousel constructor */
+function newCarousel({ _carouselElement }) {
+  const _images = _carouselElement.querySelectorAll('img');
+  let _index = 0;
+  _images[_index].style.display = 'block';
 
+  _carouselElement.getElementsByClassName('left-button')[0].addEventListener('click', () => {
+    _images[_index].style.display = 'none';
+    _index = (_index + _images.length - 1) % _images.length;
+    _images[_index].style.display = 'block';
+  });
+
+  _carouselElement.getElementsByClassName('right-button')[0].addEventListener('click', () => {
+    _images[_index].style.display = 'none';
+    _index = (_index + 1) % _images.length;
+    _images[_index].style.display = 'block';
+  });
 }
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+newCarousel({ _carouselElement: document.querySelector('.carousel') });
