9aea889976b793db2cf1751dce4a7a9b88a2bacd
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-12 12:08:10 -0400

    initial setup of cards script

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,6 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+// Send an HTTP GET request to the following address:
+//   https://lambda-times-backend.herokuapp.com/articles
 // Stduy the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
@@ -17,3 +18,26 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+(function () {
+    function createArticleCard() {
+        var cardElement = document.createElement('div');
+        var headline = document.createElement('div');
+        var author = document.createElement('div');
+        var imgContainer = document.createElement('div');
+        var img = document.createElement('img');
+        var span = document.createElement('span');
+        cardElement.className = 'card';
+        cardElement.appendChild(headline);
+        headline.className = 'headline';
+        // headline.textContent = ;
+        cardElement.appendChild(author);
+        author.className = 'author';
+        author.appendChild(imgContainer);
+        imgContainer.className = 'img-container';
+        imgContainer.appendChild(img);
+        // img.src = ;
+        author.appendChild(span);
+        // span.textContent = ;
+        return cardElement;
+    }
+})();
---
+++ components/Cards/index.ts
@@ -0,0 +1,50 @@
+// STEP 3: Create Article cards.
+// -----------------------
+// Send an HTTP GET request to the following address:
+//   https://lambda-times-backend.herokuapp.com/articles
+// Stduy the response data you get back, closely.
+// You will be creating a component for each 'article' in the list.
+// This won't be as easy as just iterating over an array though.
+// Create a function that will programmatically create the following DOM component:
+//
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
+//
+// Create a card for each of the articles and add the card to the DOM.
+(() => {
+  function createArticleCard(): HTMLDivElement {
+    const cardElement = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+
+    cardElement.className = 'card';
+
+    cardElement.appendChild(headline);
+    headline.className = 'headline';
+    // headline.textContent = ;
+
+    cardElement.appendChild(author);
+    author.className = 'author';
+
+    author.appendChild(imgContainer);
+    imgContainer.className = 'img-container';
+
+    imgContainer.appendChild(img);
+    // img.src = ;
+
+    author.appendChild(span);
+    // span.textContent = ;
+
+    return cardElement;
+  }
+})();
